9991bba4ac931ee94d4076563f91d96d165e33e3
aallbright <aallbrig@gmail.com>
2022-01-27 21:38:52 -0500

    Initialize empty unity project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Test Building Unity ARKit using local Game.Ci build
---
+++ scripts/activate-unity.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TARGET_DIR="${SCRIPT_DIR}/../unity/unity-arkit-cicd"
+
+
